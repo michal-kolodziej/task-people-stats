a8fd0e48ea4633a31bd90230a86fea97b9305a2c
Michał Kołodziej <kolothej@gmail.com>
2020-11-06 18:02:17 +0100

    Added lastName field

---
+++ Main.java
@@ -0,0 +1,27 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+
+class PeopleStats {
+
+}
+
+class Person {
+    private final String firstName;
+    private final String lastName;	
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+	this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public String getLastName() {
+	return lastName;
+    }
+}
